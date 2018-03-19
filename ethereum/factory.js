d3a911972324a66b3d083283da2ad312eecd5780
bearski <santosh.sewlal@gmail.com>
2018-03-19 22:46:46 +0200

    typos

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,8 +1,8 @@
 import web3 from './web3';
-import CampaignFactory from './build/DriverFactory.json';
+import DriverFactory from './build/DriverFactory.json';
 
 const instance = new web3.eth.Contract(
-  JSON.parse(CampaignFactory.interface),
+  JSON.parse(DriverFactory.interface),
   '0x5Ab6120a3fd29cB6f275B4cB6D2e24ba54186535'
 );
 
